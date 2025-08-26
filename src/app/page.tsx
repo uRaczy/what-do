4c9cd9ef368bbd1c9dccdeca7898bb4357ece21e
Adrian Rak <35142968+uRaczy@users.noreply.github.com>
2025-08-26 01:22:54 +0000

    add class dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+    <div className="dark grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-8 bg-black sm:items-start dark:bg-white">
         <Image
-          className="dark:invert"
+          className="invert dark:invert-0"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
