da391231a0fbce2b016dfbdf74fade7ce728b502
uRaczy <raczy7@gmail.com>
2025-09-10 20:53:45 +0000

    Add skeleton

--- src/components/Organisms/Navbar.tsx
+++ src/components/Organisms/Navbar.tsx
@@ -3,6 +3,10 @@ import { DarkModeToggle } from "../Atoms";
 export const Navbar = () => {
   return (
     <header className="h-14 bg-gray-300 dark:bg-gray-700">
+      <h1>LOGO</h1>
+      <h2>TODO</h2>
+      <h2>SQL List</h2>
+      <h2>NoSQL List</h2>
       <DarkModeToggle />
     </header>
   );
