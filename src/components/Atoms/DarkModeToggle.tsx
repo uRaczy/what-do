f0dd4f237af83497de5933f9c5bafaeb7e56b7c6
Adrian Rak <35142968+uRaczy@users.noreply.github.com>
2025-09-12 12:03:46 +0000

    add list skeleton

--- src/components/Atoms/DarkModeToggle.tsx
+++ src/components/Atoms/DarkModeToggle.tsx
@@ -5,7 +5,7 @@ import { useDarkMode } from "@/utils/index";
 export const DarkModeToggle = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   return (
-    <div className="bg-gray-700" onClick={toggleDarkMode}>
+    <div className="cursor-pointer bg-gray-700" onClick={toggleDarkMode}>
       {/* Toggle default in the middle, after localStorage loads it switches left or right */}
       {isDarkMode === null ? null : isDarkMode ? "🌙" : "☀️"}
     </div>
--- src/components/Organisms/Navbar.tsx
+++ src/components/Organisms/Navbar.tsx
@@ -3,11 +3,11 @@ import { DarkModeToggle } from "../Atoms";
 export const Navbar = () => {
   return (
     <header className="flex h-10 items-center justify-between bg-gray-300 px-4 dark:bg-gray-700">
-      <h1>LOGO</h1>
+      <h1 className="cursor-pointer">LOGO</h1>
       <nav className="flex gap-3">
-        <h2>TODO</h2>
-        <h2>SQL List</h2>
-        <h2>NoSQL List</h2>
+        <h2 className="cursor-pointer">TODO</h2>
+        <h2 className="cursor-pointer">SQL List</h2>
+        <h2 className="cursor-pointer">NoSQL List</h2>
       </nav>
       <DarkModeToggle />
     </header>
